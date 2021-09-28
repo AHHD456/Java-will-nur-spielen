d661308dfb8b3cecfbfa93d783a9231dd225257e
alber <alber@DESKTOP-VC1PQOL>
2021-09-28 15:13:35 +0200

    28_09_1

--- Java will nur spielen/src/TestVertex.java
+++ Java will nur spielen/src/TestVertex.java
@@ -1,26 +1,56 @@
+import java.util.Scanner;
 
 public class TestVertex {
 
 	public static void main(String[] args) {
 
+		byte vertex;
+
 		Vertex v1 = new Vertex(17, 4);
 		Vertex v2 = new Vertex(42, 0);
-		Vertex v3 = new Vertex(0, 0);
+		Vertex v3 = v1.skalarMult(42.42);
 		
-		System.out.println(v1);
-		System.out.println(v2);
-		System.out.println(v3);
+		
+		
+		Scanner eingabe = new Scanner(System.in);
+		System.out.println("Welche Vertex Infos willst du?");
+		vertex = eingabe.nextByte();
 
-		hazweisieben();
 		
-	
+
+		if (vertex == 1) {
+			System.out.println(v1);
+			System.out.println(v1.length());
+		}
+		if (vertex == 2) {
+			System.out.println(v2);
+			System.out.println(v2.length());
+		}
+		if (vertex == 3) {
+			System.out.println(v3);
+			System.out.println(v3.length());
+		}
+
+		// hazweisieben();
+		// EntfernungUrsprung();
+
 	}
-   public static void hazweisieben() {
-	   
-	   System.out.println(("a"+ 1)+2);
-	   System.out.println("a"+(1+2));
-	   
-	   
-	   
-   }
+	/*
+	 * public static void hazweisieben() {
+	 * 
+	 * System.out.println(("a"+ 1)+2); System.out.println("a"+(1+2));
+	 * 
+	 * 
+	 * 
+	 * }
+	 * 
+	 * public static void EntfernungUrsprung() {
+	 * 
+	 * 
+	 * double EntfUr ;
+	 * 
+	 * EntfUr = Math.sqrt(v1.x*v1.x+v1.y*v1.y); System.out.println(EntfUr);
+	 * 
+	 * }
+	 */
 }
--- Java will nur spielen/src/Vertex.java
+++ Java will nur spielen/src/Vertex.java
@@ -1,6 +1,4 @@
 
-
-
 public class Vertex {
 
 	public double x;
@@ -12,9 +10,38 @@ public class Vertex {
 		this.y = y;
 
 	}
-   public String toString() {
-	   
-	   return " x=" +x+", y=" +y;
-	   
-   }
+
+	public String toString() {
+
+		return " x=" + x + ", y=" + y;
+
+	}
+
+	public double length() {
+
+		return Math.sqrt(x * x + y * y);
+	}
+
+	public Vertex skalarMult(double s) {
+		return new Vertex(x * s, y * s);
+	}
+
+	public void skalarMultMod(double s) {
+		x = s * x;
+		y = s * y;
+
+	}
+
+	public Vertex add(Vertex v2) {
+		return new Vertex( x + v2.x , y + v2.y);
+		
+	}
+	
+	public void addMod(Vertex v2) {
+		
+		x = v2.x + x;
+		y = v2.y + y ;
+	}
+	
+	
 }
